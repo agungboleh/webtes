80277dd5b85e8491abbe46c12fee6602969c1ef0
agungboleh <agungbolehnet@gmail.com>
2026-05-18 08:35:00 +0700

    Deploy with basePath /webtes

---
+++ _next/static/p12csUj_lwMWeVpPSN6DX/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/webtes//_next/:path+",
+        "destination": "/webtes/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
